56f4f400db0660ff64b1e5861bf858f9cbc3ab77
Joel Chretien <joelchretien@gmail.com>
2018-11-13 22:08:19 -0500

    Renamed specs whose tested objects have bene renamed

--- spec/queries/transaction_reporting/transaction_dashboard_service_spec.rb
+++
@@ -1,9 +0,0 @@
-describe TransactionReporting::TransactionDashboardService do
-  context "when it is called" do
-    it "aggregates the information from other services" do
-      # TODO: Add in spec for this.
-      # timeline_items = double('timeline_items')
-      # allow(
-    end
-  end
-end
